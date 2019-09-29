9239fc11966cd34f2ccae003241b0b9fb040bef2
benjicaulfield <39502512+benjicaulfield@users.noreply.github.com>
2019-09-29 10:16:11 -0500

    Update new_minard_viz.py

--- new_minard_viz.py
+++ new_minard_viz.py
@@ -25,63 +25,52 @@ m = folium.Map(location=[54,31],
                zoom_start=5,
                control_scale=True)
 
-def draw_troops(mapobj, df):
-    coords = []
-    for i, row in gdf_troops.iterrows():
-        coords.append([row.geometry.y, row.geometry.x])
-    for coord in coords:
-        folium.CircleMarker(location = coord,
-                            radius = 2.5,
-                            fill = True,
-                            fill_color = 'black',
-                            fill_opacity = 0.75,
-                            color = 'whitesmoke',
-                            weight = 0.5).add_to(mapobj)
-        folium.PolyLine(coords, color="black", weight=1.0).add_to(mapobj)
-    
-    return mapobj
-
 def draw_polygons(mapobj):
     
-    m = folium.Map(location=[54,31],
-               tiles='OpenStreetMap',
-               zoom_start=5,
-               control_scale=True)
-    
-    
     for i, row in gdf_troops.iterrows():
-        while i == len(gdf_troops) - 1:
+        
+        if i == len(gdf_troops) - 1:
+            pass
+        
+        else:
+            
             x1, y1 = coords[i]
             x2, y2 = coords[i+1]
 
-
             if x2 - x1 != 0:
-                m = (y2 - y1) / (x2 - x1)
+                #slope of line
+                slope = (y2 - y1) / (x2 - x1)
                 #negative inverse slope (perpindicular angle)
-                m_inv = (-1 / m)
+                slope_inv = (-1 / slope)
             else:
-                m_inv = 0
+                slope_inv = 0
 
-            c = 1 / math.sqrt(1 + (m_inv**2))
-            s = m_inv / math.sqrt(1 + (m_inv**2))
+            #cosine, sine to find x, y coordinates of polygon corners
+            c = 1 / math.sqrt(1 + (slope_inv**2))
+            s = m_inv / math.sqrt(1 + (slope_inv**2))
 
+            #find width of polygon (magnitude of survivors)
             gdf_survivors = gdf_troops['survivors']
-            gdf_direction = gdf_troops['direction']
             buffer = (gdf_survivors[i] / 34000) *.03
-            
+
             x1u, y1u = (x1 + (buffer*c)), (y1 + (buffer*s))
             x1d, y1d = (x1 - (buffer*c)), (y1 - (buffer*s))
             x2u, y2u = (x2 + (buffer*c)), (y2 + (buffer*s))
             x2d, y2d = (x2 - (buffer*c)), (y2 - (buffer*s))
 
-            lat_point_list = [x1u, x1d, x2d, x2u]
-            lon_point_list = [y1u, y1d, y2d, y2u]
+            lat_point_list = [(x1 + (buffer*c)), (x1 - (buffer*c)), (x2 - (buffer*c)), (x2 + (buffer*c))]
+            lon_point_list = [(y1 + (buffer*s)), (y1 - (buffer*s)), (y2 - (buffer*s)), (y2 + (buffer*s))]     
 
-            polygon_geom = Polygon(zip(lon_point_list, lat_point_list))
-            crs = {'init': 'epsg:4326'}
-              
-            polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom])
-            
 
-        return mapobj
+            geometry = Polygon(zip(lon_point_list, lat_point_list))
+            poly = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[geometry])       
+
+            folium.GeoJson(poly).add_to(mapobj)
+
+
+
+
+
+    return mapobj
+    
     
